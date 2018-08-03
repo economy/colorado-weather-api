55493e728954373030e4f110b730e778ae1d7fbc
economy <the.economy@gmail.com>
2018-08-02 20:48:13 -0700

    sqlite3 backend for updates

--- api.py
+++ api.py
@@ -2,37 +2,75 @@
 # Imports
 #----------------------------------------------------------------------------#
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 import logging
 from logging import Formatter, FileHandler
 import os
 import pandas as pd
 import pickle
+from datetime import datetime
+import sqlite3
 
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
 
 app = Flask(__name__)
-#app.config.from_object('config')
+DATABASE = './tmp_db.db'
 
 #----------------------------------------------------------------------------#
 # Controllers.
 #----------------------------------------------------------------------------#
 
 
-@app.route('/get', methods=['GET', 'POST'])
-def show_temps():
-	
+@app.route('/temps', methods=['GET', 'POST'])
+def temps():
+
+	conn = get_db()	
+	df = pd.read_sql_query('select * from temps', conn)
+
 	# optionally, specify how many records you want
 	recs = request.args.get('records')
 	if recs:
 		recs = int(recs)
-		a = df.head(recs).to_json(orient='records')
+		response = df.head(recs).to_json(orient='records')
 	else:
-		a = df.sort_values('celc', ascending=False).to_json(orient='records')	
+		response = df.sort_values('added', ascending=False).to_json(orient='records')	
 
-	return a, 200
+	return response, 200
+
+@app.route('/add_temp', methods=['GET', 'POST'])
+def add_temp():
+
+	conn = get_db()
+	df = pd.read_sql_query('select * from temps', conn)
+	
+	if request.method == 'GET':
+		lat = request.args.get('lat')
+		long = request.args.get('long')
+		temp = request.args.get('temp')
+	else:
+		lat = request.form['lat']
+		long = request.form['long']
+		temp = request.form['temp']
+
+	try:
+		updated_df = df.append([lat, long, temp, str(datetime.now())], ignore_index=True)
+		cursor = conn.cursor()
+		cursor.execute('''INSERT INTO temps(lat, long, celc, added)
+                	VALUES(?,?,?,?)''', (lat, long, temp, str(datetime.now())))
+		conn.commit()
+	except IOError:
+		print("Unable to add the record given input!")
+
+	a = updated_df.shape[0]	
+	response = "Record added. Total records for Colorado: {:,}".format(a)
+
+	return response, 200
+
+@app_route('/update_temp', methods=['GET', 'POST', 'PUT'])
+def update_temp():
+	
 
 # Error handlers.
 
@@ -80,7 +118,50 @@ def load_data(f='./weather_data.pkl', colorado_only=True):
 	# we only care about celsius, so ignore fahr column
 	df = df.drop('fahr', axis=1)
 
+	# add a datetime field to keep track of recent record additions
+	df['added'] = str(datetime.now())
+
 	return df	
+
+def get_db():
+	db = getattr(g, '_database', None)
+	if db is None:
+		db = g._database = sqlite3.connect(DATABASE)
+	return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+	db = getattr(g, '_database', None)
+	if db is not None:
+		db.close()
+
+def db_load(conn, df):
+
+	cursor = conn.cursor()
+
+	# drop old if exists
+	cursor.execute(
+		'''
+		DROP TABLE IF EXISTS temps
+		'''
+	)
+	conn.commit()
+
+	# create table in memory
+	cursor.execute(
+		'''
+		CREATE TABLE temps(lat REAL, long REAL, celc REAL, added TEXT)
+		'''
+	)
+	conn.commit()
+
+	# load df into table
+	cursor.executemany(
+		'''
+		INSERT INTO temps(lat, long, celc, added) VALUES(?,?,?,?)
+		''', df.to_records(index=False)
+	)
+	conn.commit()
 
 def fahr_to_celc(fahr):
 	
@@ -93,8 +174,13 @@ def fahr_to_celc(fahr):
 
 if __name__ == '__main__':
 
-	# load dataframe on init
+	# load dataframe on init to sqlite3
 	df = load_data()
+
+	# place data in temp db
+	with app.app_context():
+		conn = get_db()
+		db_load(conn, df)
 
 	port = int(os.environ.get('PORT', 5555))
 	app.run(host='0.0.0.0', port=port)
